05afe9514e073a26a32c9947c28f4d8b4f9351fa
Sandeep Appikonda <sandeep@appikonda.in>
2014-07-23 19:44:15 -0700

    changes made to  superadd.js file

--- superadd.js
+++ superadd.js
@@ -1,6 +1,37 @@
 // Copyright 2014 Sandeep Appikonda
 
 function superAdd(firstArg, secondArg){
-  // Implement...
+   
+   
+   if (typeof(firstArg) === "number")
+   {
+   	  return firstArg + secondArg;
+   }
+   else if(typeof(firstArg) === "string")
+   {
+   	  return firstArg +" "+ secondArg;
+   }
+   else if( Array.isArray(firstArg) ){
+   	 return  firstArg.concat(secondArg);
+   }
+   else
+   {
+   	  return addObjects(firstArg,secondArg);
+   }
 
 } 
+
+// This function will add two objects and returns the result object
+
+function addObjects(firstObject, secondObject){
+	var resultObject = {};
+	
+	for (var key in firstObject) {
+		resultObject[key] = firstObject[key]
+	}
+	for (var key in secondObject) {
+		resultObject[key] = secondObject[key]
+	}
+
+	return resultObject;
+}
